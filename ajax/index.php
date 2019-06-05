6270b41b4b36b6b1c5966762e280b1540e80a848
jhygt46 <infolifeoperacional@gmail.com>
2019-06-05 16:45:31 -0400

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -3,7 +3,7 @@
     require_once "../class/core_class.php";
 
     $core = new Core();
-    $info = $core->get_info_despacho();
+    $info = $core->get_info_despacho($_POST["lat"], $_POST["lng"]);
     echo json_encode($info);
 
 ?>
