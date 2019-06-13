8036ae020076560f6e793d858ab756a700d3001d
jhygt46 <infolifeoperacional@gmail.com>
2019-06-13 11:40:04 -0400

    aa

--- class/core_class.php
+++ class/core_class.php
@@ -206,7 +206,12 @@ class Core{
 
         $pedido_code = $_GET["code"];
         $file = $this->dir_info."pedidos/".$pedido_code.".json";
-        
+        $aux_conf = json_decode(file_get_contents($this->dir_info."config.json"));
+
+        if(file_exists($this->dir_info."versiones/".$config["info"]) && $config["actualizar"] == 0){
+            $info['bue'] = json_decode(file_get_contents($this->dir_info."versiones/".$config["info"]));
+        }
+
         if(file_exists($file)){
             $data = json_decode(file_get_contents($this->dir_info."pedidos/".$pedido_code.".json"));
             $fecha = $data->{'pedido'}->{'fecha'};
@@ -214,7 +219,7 @@ class Core{
             if($diff < 86400){
                 $info['op'] = 1;
                 $info['data'] = $data;
-                $info['js_data'] = json_decode(file_get_contents($this->file_info))->{'code'}.".js";
+                $info['code'] = json_decode(file_get_contents($this->file_info))->{'code'}.".js";
             }else{
                 $info['op'] = 2;
             }
