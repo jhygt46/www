09af4cc11e329be1cf76a6bd65c91e7b38fe14ae
jhygt46 <infolifeoperacional@gmail.com>
2019-06-17 18:50:51 -0400

    aa

--- class/core_class.php
+++ class/core_class.php
@@ -189,7 +189,7 @@ class Core{
 
         $config = $this->get_config();
         $polygons = json_decode(file_get_contents($this->dir_info."polygon/".$config["polygon"]));
-        return $this->dir_info."polygon/".$config["polygon"];
+        return json_encode($_SERVER);
 
         $precio = 9999999;
         $info['op'] = 2;
