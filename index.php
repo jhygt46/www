7661064af26e2b3b3b8b8737f66b079fcffa7eb0
jhygt46 <infolifeoperacional@gmail.com>
2019-06-17 14:59:16 -0400

    aa

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     if($_POST["accion"] == "xS3w1Dm8Po87Wltd"){
         $core->actualizar();
+        die("BUENA NELSON");
     }
     if($_POST["accion"] == "uDr72rE2kIt9z5bF"){
         $core->volver();
