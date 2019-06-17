2cdcbb6516782e90fe0b378337682877f13f2345
jhygt46 <infolifeoperacional@gmail.com>
2019-06-17 18:12:00 -0400

    aa

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 
     $info = $core->get_data();
 
+    /*
     if($_SERVER['HTTP_HOST'] == "35.192.157.227"){
         echo "<pre>";
         print_r($info);
         echo "</pre>";
         exit;
     }
-
-    
+    */    
 
     if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && $info->{'ssl'} == 1) {
         $location = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
