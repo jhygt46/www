2dd2c6c897ad427480a0dbea8b94cc709a1d6b11
jhygt46 <infolifeoperacional@gmail.com>
2019-06-19 18:13:44 -0400

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -4,17 +4,18 @@
         require_once "../class/core_class.php";
         $core = new Core();
         $info = $core->get_info_despacho($_POST["lat"], $_POST["lng"]);
+        echo json_encode($info);
     }
     if($_POST["accion"] == "enviar_pedido"){
         require_once "../class/core_class.php";
         $core = new Core();
         $info = $core->enviar_pedido();
+        echo json_encode($info);
     }
     if($_POST["accion"] == "enviar_error"){
         require_once "../class/core_class.php";
         $core = new Core();
         $info = $core->enviar_error();
     }
-    echo json_encode($info);
 
 ?>
