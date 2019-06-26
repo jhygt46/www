2a038527fb9b0c35b6a2324454ea5d74d01e8393
jhygt46 <infolifeoperacional@gmail.com>
2019-06-25 20:28:44 -0400

    aa

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
     }
 
     $info = $core->get_data();
+    echo "<pre>";
+    print_r($info);
+    echo "</pre>";
+
     if($info === null){
         die("<table border='0' width='100%' height='100%'><tr><td align='center' valign='middle'>Sitio no disponible</td></tr></table>");
     }else{
