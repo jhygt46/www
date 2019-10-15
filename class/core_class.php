76789c74bbaae94369fc411f8e535b767862524d
jhygt46 <infolifeoperacional@gmail.com>
2019-10-15 14:44:22 -0300

    aa

--- class/core_class.php
+++ class/core_class.php
@@ -123,10 +123,12 @@ class Core{
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($send));
         if(!curl_errno($ch)){
             $data = json_decode(curl_exec($ch));
+            /*
             echo "<pre>";
             print_r($data);
             echo "</pre>";
             exit;
+            */
             curl_close($ch);
             if($data->{'op'} == 1){
                 if(!is_dir($this->dir)){
