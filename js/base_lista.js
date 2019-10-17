ccf498b5e8b52a525399f6d004ae85c6a8254fa1
jhygt46 <infolifeoperacional@gmail.com>
2019-10-17 15:57:20 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -831,18 +831,15 @@ function aux_fecha(){
         for(var i=0, ilen=data.locales.length; i<ilen; i++){
             for(var j=0, jlen=data.locales[i].horarios.length; j<jlen; j++){
 
-                console.log(data.locales[i].horarios[j]);
-
                 var dia_ayer = fecha_ayer.getDay() > 0 ? fecha_ayer.getDay() : 7 ;
                 var dia_hoy = fecha_actual.getDay() > 0 ? fecha_actual.getDay() : 7 ;
                 var hora_ini = (data.locales[i].horarios[j].hora_ini * 3600 + data.locales[i].horarios[j].min_ini * 60) * 1000;
                 var hora_fin = (data.locales[i].horarios[j].hora_fin * 3600 + data.locales[i].horarios[j].min_fin * 60) * 1000;
 
                 if(dia_ayer >= data.locales[i].horarios[j].dia_ini && dia_ayer <= data.locales[i].horarios[j].dia_fin){
-                    console.log("ENTRO HORA AYER #2");
+                    console.log("ENTRO HORA AYER #1");
                     var time_ayer_ini = new Date(fecha_ayer_00 + hora_ini).getTime();
                     var time_ayer_fin = new Date(fecha_ayer_00 + hora_fin).getTime();
-                    console.log(time_ayer_ini+"//"+time_ayer_fin);
                     if(datetime_actual >= time_ayer_ini && datetime_actual <= time_ayer_fin){
                         console.log("ENTRO HORA AYER #2");
                     }
@@ -851,14 +848,9 @@ function aux_fecha(){
                     console.log("ENTRO HORA HOY #1");
                     var time_hoy_ini = new Date(fecha_hoy_00 + hora_ini).getTime();
                     var time_hoy_fin = new Date(fecha_hoy_00 + hora_fin).getTime();
-                    console.log(new Date(datetime_actual));
-                    console.log(new Date(fecha_hoy_00 + hora_ini));
-                    console.log(new Date(fecha_hoy_00 + hora_fin));
-                    /*
                     if(datetime_actual >= time_hoy_ini && datetime_actual <= time_hoy_fin){
                         console.log("ENTRO HORA HOY #2");
                     }
-                    */
                 }
 
             }
