4c626ab1369c303171ff90182f7ff9604d7330ac
jhygt46 <infolifeoperacional@gmail.com>
2019-10-17 22:56:31 -0300

    aa

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     echo "<pre>";
     print_r($info);
     echo "</pre>";
+    exit;
 
     if($info === null){
         die("<table border='0' width='100%' height='100%'><tr><td align='center' valign='middle'>Sitio no existe</td></tr></table>");
