0867fd46313f51559345040dcc140d0674d19c36
jhygt46 <infolifeoperacional@gmail.com>
2019-10-19 18:36:25 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -60,6 +60,7 @@ function html_paginas(pagina){
 // HTML MODALS //
 function create_html_categorias(obj){
 
+    console.log(obj);
 
     var Div = document.createElement('div');
     Div.className = 'categoria';
