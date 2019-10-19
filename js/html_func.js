90f47cce6cb1107779f5cf5f8efdda932b516cd9
jhygt46 <infolifeoperacional@gmail.com>
2019-10-19 15:25:34 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -59,8 +59,7 @@ function create_html_categorias(obj){
     var Div = document.createElement('div');
     Div.className = 'categoria';
     Div.style.backgroundImage = 'url("/data/'+code+'/'+obj.image+'")';
-    console.log("alto: " + data.config.alto);
-
+    Div.style.height = parseInt(380 * data.config.alto / 100);
 
     var Divm = document.createElement('div');
 
