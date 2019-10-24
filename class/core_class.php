87b8c699a90ea155d9052af5d46063474b03050f
jhygt46 <infolifeoperacional@gmail.com>
2019-10-24 12:01:49 -0300

    aa

--- class/core_class.php
+++ class/core_class.php
@@ -106,6 +106,7 @@ class Core{
         echo "<pre>";
         print_r($config);
         echo "</pre>";
+        exit;
         if(file_exists($this->dir_info."versiones/".$config["info"]) && $config["actualizar"] == 0){
             return json_decode(file_get_contents($this->dir_info."versiones/".$config["info"]));
         }else{
