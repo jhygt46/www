bc52533f5a819298861333a737bbcea807c19cc7
jhygt46 <infolifeoperacional@gmail.com>
2019-10-27 19:07:10 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -1,14 +1,9 @@
 <?php
 
     $file = explode("/", $_SERVER["REQUEST_URI"]);
-    echo "<pre>";
-    print_r($file);
-    echo "</pre>";
-    exit;
-
-    if($file[count($file) - 1] != ""){
+    if($file[count($file) - 1] != "detalle.php" && count($file) == 3){
         header('HTTP/1.1 404 Not Found', true, 404);
-        include(DIR.'errors/404.html');
+        //include(DIR.'errors/404.html');
         exit;
     }
 
