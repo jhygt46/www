faeb28bd1ec78613ea5ad081156ab70f8a69980d
jhygt46 <infolifeoperacional@gmail.com>
2019-10-28 18:35:45 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -1,6 +1,12 @@
 <?php
 
     $file = explode("/", $_SERVER["REQUEST_URI"]);
+
+    echo "<pre>";
+    print_r($file);
+    echo "</pre>";
+    exit;
+
     if($file[count($file) - 1] == "detalle.php" && count($file) == 2){
         header('HTTP/1.1 404 Not Found', true, 404);
         include('errors/404.html');
@@ -11,7 +17,7 @@
     $core = new Core();
     $info = $core->ver_pedido();
     
-    if($info['op'] == 1){
+    if($info['op'] == 1){ 
 
         $pedido = $info['data']->{'pedido'};
         $puser = $info['data']->{'puser'};
