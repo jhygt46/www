ae9455cd814369bcb66376de268d25453a522f87
jhygt46 <infolifeoperacional@gmail.com>
2019-11-08 15:24:02 -0300

    aa

--- detalle.php
+++ detalle.php
@@ -9,6 +9,10 @@
     require_once "class/core_class.php";
     $core = new Core();
     $info = $core->ver_pedido();
+    
+    echo "<pre>";
+    print_r($info);
+    echo "</pre>";
     exit;
 
     if($info['op'] == 1){
