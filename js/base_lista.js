a3eceb762f69f638481afd931a4ef61fd5faa36b
jhygt46 <infolifeoperacional@gmail.com>
2019-11-14 22:00:14 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -571,12 +571,14 @@ function get_puser(){
 function set_puser(puser){
     localStorage.setItem("p_user", JSON.stringify(puser));
 }
+/*
 function get_pdir(){
     return JSON.parse(localStorage.getItem("p_dir")) || { id_pdir: 0, direccion: '', calle: '', num: '', depto: '', lat: 0, lng: 0, verificado: 0 };
 }
 function set_pdir(pdir){
     localStorage.setItem("p_dir", JSON.stringify(pdir));
 }
+*/
 function get_carro(){
     return JSON.parse(localStorage.getItem("carro")) || [];
 }
@@ -662,6 +664,7 @@ function get_productos_categoria(id_cae){
 function borrar_carro(){
     localStorage.setItem("carro", null);
     localStorage.setItem("carro_promos", null);
+    borrar_pedido();
     $('.cantcart_num').html(0);
 }
 function ver_pagina(id){
