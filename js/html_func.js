88e9a71033d8b0c211b96ff4953466ce2c041656
jhygt46 <infolifeoperacional@gmail.com>
2019-11-17 23:18:39 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -180,7 +180,7 @@ function create_html_producto(id, detalle){
                 descripcion.innerHTML = aux.descripcion;
             }
             if(aux.disponible == 1){
-                descripcion.innerHTML = aux.descripcion + '<br/><p style="color: #933">Producto no disponible</p>';
+                descripcion.innerHTML = aux.descripcion + '<br/><p style="color: #933; font-size: 10px">Producto no disponible</p>';
             }
             Div.appendChild(descripcion);
             
