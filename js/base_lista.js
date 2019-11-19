59861fc606774bd57553f5d97325e62c82202e33
jhygt46 <infolifeoperacional@gmail.com>
2019-11-19 10:39:36 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -157,8 +157,9 @@ function imprimir_productos_modal(id){
         for(var j=0, jlen=productos.length; j<jlen; j++){
             var producto = get_producto(productos[j]);
             if(producto.disponible != 2 && categoria.detalle_prods == 0){
-                html.append(create_html_producto(producto));
+                //html.append(create_html_producto(producto));
                 //html.append(create_html_producto2(producto));
+                html.append(create_html_producto3(producto));
             }
         }
     }
