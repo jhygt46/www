8a3ca5603a0dc649e1e3d4cd8b73ad5a650a1179
jhygt46 <infolifeoperacional@gmail.com>
2019-11-19 18:41:31 -0300

    aa

--- index.php
+++ index.php
@@ -19,6 +19,13 @@
     }
 
     $info = $core->get_data();
+    if($_GET["mode2"] == "developer"){
+        echo "<pre>";
+        print_r($info);
+        echo "</pre>";
+        exit;
+    }
+
     if($info === null){
         die("<table border='0' width='100%' height='100%'><tr><td align='center' valign='middle'>Sitio no existe</td></tr></table>");
     }else{
