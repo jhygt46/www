f1f1800f7e124a3c8b672b26e10b202098587311
jhygt46 <infolifeoperacional@gmail.com>
2019-11-24 11:14:07 -0300

    aa

--- index.php
+++ index.php
@@ -218,12 +218,17 @@
                             </div>
                         </div>
                     </div>
+
+                    
                     <div class="modal modal_dim1 vhalign hide modal_pagina">
                         <div class="cont_modal">
                             <div onclick="close_that(this)" class="close material-icons">close</div>
                             <div class="cont_info"></div>
                         </div>
                     </div>
+
+
+
                     <div class="modal modal_dim1 vhalign hide modal_carta">
                         <div class="cont_modal">
                             <div class="titulo"><div class="cont_titulo valign"><h1></h1><h2></h2></div></div>
