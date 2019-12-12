03890bec8ebac67bcb2b6c297b4c77826dd3b0b3
jhygt46 <infolifeoperacional@gmail.com>
2019-12-12 18:02:10 -0300

    aa

--- index.php
+++ index.php
@@ -40,7 +40,9 @@
     }
     
     $horas = (isset($_GET['horas'])) ? $_GET['horas'] : 0 ;
-    $css = (isset($_GET['css'])) ? "r" : "r" ;
+    $css = (isset($_GET['css'])) ? "r" : "" ;
+    $rand = rand(0, 1);
+    $css = ($rand == 0) ? "r" : "" ;
 
 ?>
 
@@ -48,7 +50,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title><?php echo $info->{"titulo"}; ?></title>
+        <title><?php echo $info->{"titulo"}." - ".$css; ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
