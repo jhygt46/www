18752906fe0d9872c698438f59295bf97cf77836
jhygt46 <infolifeoperacional@gmail.com>
2019-12-12 16:26:12 -0300

    aa

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-    echo "<pre>";
-    print_r($_SERVER);
-    echo "</pre>";
-
     if(isset($_POST["test"]) && $_POST["test"] == "Dw7k2s_hKi5sqPs8"){
         die("hjS3r%mDs-5gYa6ib_5Ps");
     }
