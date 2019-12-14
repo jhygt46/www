1e7f13ceef545775ca3798a9b707a3320076f36d
jhygt46 <infolifeoperacional@gmail.com>
2019-12-13 22:40:21 -0300

    aa

--- class/core_class.php
+++ class/core_class.php
@@ -229,11 +229,9 @@ class Core{
                 if(file_put_contents($this->dir_data."data/".$data->{"info"}->{"code"}."/index.js", "var data=".json_encode($data->{"data"}))){
                     $categorias = $data->{"data"}->{"catalogos"}[0]->{"categorias"};
                     for($i=0; $i<count($categorias); $i++){
-                        if(strlen($categorias[$i]->{"image"}) == 25){
-                            if(!file_exists($this->dir_data."data/".$data->{"info"}->{"code"}."/".$categorias[$i]->{"image"})){
-                                if(!file_put_contents($this->dir_data."data/".$data->{"info"}->{"code"}."/".$categorias[$i]->{"image"}, file_get_contents("http://www.misitiodelivery.cl/images/categorias/".$categorias[$i]->{"image"}))){
-                                    $this->enviar_error(16, "No se pudo guardar las imagenes de categorias ".$this->host);
-                                }
+                        if(!file_exists($this->dir_data."data/".$data->{"info"}->{"code"}."/".$categorias[$i]->{"image"})){
+                            if(!file_put_contents($this->dir_data."data/".$data->{"info"}->{"code"}."/".$categorias[$i]->{"image"}, file_get_contents("http://www.misitiodelivery.cl/images/categorias/".$categorias[$i]->{"image"}))){
+                                $this->enviar_error(16, "No se pudo guardar las imagenes de categorias ".$this->host);
                             }
                         }
                     }
