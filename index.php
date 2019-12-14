802732e37d6401124e7492777fc1936835888f52
jhygt46 <infolifeoperacional@gmail.com>
2019-12-14 18:53:00 -0300

    aa

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+    if(isset($_GET["cat"])){
+        echo "CAT: ".$_GET["cat"]."<br/>";
+    }
+    if(isset($_GET["pro"])){
+        echo "PRO: ".$_GET["pro"]."<br/>";
+    }
+
     if(isset($_POST["test"]) && $_POST["test"] == "Dw7k2s_hKi5sqPs8"){
         die("hjS3r%mDs-5gYa6ib_5Ps");
     }
