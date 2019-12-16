646220bf7eb5bc75ba31ca9f6daa26b6af3ea45d
jhygt46 <infolifeoperacional@gmail.com>
2019-12-16 17:51:56 -0300

    aa

--- js/html_func.js
+++ js/html_func.js
@@ -1,11 +1,9 @@
 // IMPRIME CATEGORIAS Y PAGINAS EN HOME //
 function html_home_categorias(obj, num){
 
-    console.log(obj);
-
     var Div = document.createElement('a');
     Div.className = 'botones_principales';
-    Div.setAttribute('href', "/categoria");
+    Div.setAttribute('href', "/"+obj.name+"/");
     
     if(obj.image == ""){
         Div.style.backgroundImage = 'url("/_images/cat_'+num+'.jpg")';
