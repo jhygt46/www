95241bf2d28a4879889cbf86defef21f584346dd
jhygt46 <infolifeoperacional@gmail.com>
2019-12-16 18:24:53 -0300

    aa

--- index.php
+++ index.php
@@ -14,6 +14,15 @@
 
     $info = $core->get_data();
     $url = explode("/", $_SERVER["REQUEST_URI"]);
+    $c_url = count($url) - 1;
+
+    echo "<pre>";
+    print_r($url);
+    echo "</pre>";
+
+    if($url[$c_url] == ""){
+        unset($url[$c_url]);
+    }
 
     echo "<pre>";
     print_r($url);
