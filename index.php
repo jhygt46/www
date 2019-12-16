af429dd6d78fd6bdd30a68a0428f95615d337e1d
jhygt46 <infolifeoperacional@gmail.com>
2019-12-16 16:55:28 -0300

    aa

--- index.php
+++ index.php
@@ -41,10 +41,6 @@
             exit;
         else:
             $url_cat = $core->rec_url($info->{'categorias'}, 0, $url, 1);
-            echo "<pre>";
-            print_r($url_cat);
-            echo "</pre>";
-            exit;
             if($url_cat['op'] == 1){
                 $url_cat_id = $url_cat['id'];
             }
