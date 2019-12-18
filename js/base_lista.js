f7fd52f0d78c63922a584518df0c6cfac7b28a4d
jhygt46 <infolifeoperacional@gmail.com>
2019-12-18 17:20:12 -0300

    aa

--- js/base_lista.js
+++ js/base_lista.js
@@ -225,7 +225,7 @@ function add_carro_producto(id_pro, silencio){
     set_cantidad(1);
     localStorage.setItem("carro", JSON.stringify(carro));
     if(silencio == 0){
-        if(tiene_pregunta(item_carro)){ mostrar_pregunta(carro.length - 1) }else{ hide_modal(); $('.cart_info').html("Producto Agregado"); }
+        if(tiene_pregunta(item_carro)){ mostrar_pregunta(carro.length - 1) }else{ hide_modal(); $('.cart_info').html("Producto Agregado"); console.log("Producto Agregado"); }
         history.pushState(null, '', '/');
     }
     
